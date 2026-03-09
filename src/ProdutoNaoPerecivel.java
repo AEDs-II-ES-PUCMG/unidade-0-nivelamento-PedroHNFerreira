41c51898a657510ebfedeb6d69140996bf5929be
PedroHNFerreira <nogueirapedro651@gmail.com>
2026-03-08 21:13:45 -0300

    refactory:"alteraçao de classes"

--- src/ProdutoNaoPerecivel.java
+++ src/ProdutoNaoPerecivel.java
@@ -1,16 +1,27 @@
-public class ProdutoNaoPerecivel extends Produto {
-  
+public class ProdutoNaoPerecivel extends Produto{
+    public ProdutoNaoPerecivel(String desc, double precoCusto, double margemLucro) {
+        super(desc, precoCusto, margemLucro);
+    }
 
-public ProdutoNaoPerecivel(String descricao, double precoCusto, double margemLucro) {
-	super (descricao, precoCusto, margemLucro);
-	}
+    public ProdutoNaoPerecivel(String desc, double precoCusto) {
+        super(desc, precoCusto);
+    }
 
-public ProdutoNaoPerecivel(String descricao, double precoCusto) {
-	super (descricao, precoCusto);
-	}
+    @Override
+    public double valorDeVenda() {
+        return (precoCusto * (1.0 + margemLucro));
+    }
 
-public double valorVenda() {
-		return precoCusto * (1.0 + margemLucro);
-	}
-	
+    /**
+     * Gera uma linha de texto a partir dos dados do produto. Preço e margem de lucro vão formatados com 2 casas
+     decimais.
+     * @return Uma string no formato "1; descrição;preçoDeCusto;margemDeLucro"
+     */
+    @Override
+    public String gerarDadosTexto() {
+        String precoFormatado = String.format("%.2f", precoCusto).replace(',', '.');
+        String margemFormatada = String.format("%.2f", margemLucro).replace(',', '.');
+        String linha = String.format("1;%s;%s;%s", getDescricao(), precoFormatado, margemFormatada);
+        return linha;
+    }
 }
--- src/ProdutoPerecivel.java
+++ src/ProdutoPerecivel.java
@@ -1,40 +1,50 @@
-
-    import java.time.LocalDate;
-    import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class ProdutoPerecivel extends Produto{
 
- private static final double DESCONTO = 0.25;
+    private double DESCONTO = 0.25;
+    private int PRAZO_DESCONTO = 7;
+    private LocalDate dataDeValidade;
 
- private static final int PRAZO_DESCONTO = 7;
+    public ProdutoPerecivel(String desc, double precoCusto, double margemLucro, LocalDate dataDeValidade){
+        super(desc, precoCusto, margemLucro);
+        if(dataDeValidade.isAfter(LocalDate.now())){
+            this.dataDeValidade = dataDeValidade;
+        } else {
+            throw new IllegalArgumentException("O produto está vencido!");
+        }
 
- private LocalDate dataDeValidade;
-
- public ProdutoPerecivel(String descricao, double precoCusto, double margemLucro, LocalDate validade){
-    super (descricao, precoCusto, margemLucro);
-    if (validade.isBefore(LocalDate.now())){
-        throw new IllegalArgumentException("o produto esta vencido!");
     }
-    dataDeValidade = validade;
- }
 
- @Override
- public double valorVenda() {
-    double desconto = 0d;
-    int diasValidade = LocalDate.now().until(dataDeValidade).getDays();
-    if(diasValidade<=PRAZO_DESCONTO){
-        desconto = DESCONTO;
+    @Override
+    public double valorDeVenda(){
+        LocalDate hoje = LocalDate.now();
+        LocalDate tempo_desconto = dataDeValidade.minusDays(PRAZO_DESCONTO);
+        if(hoje.isBefore(dataDeValidade) && hoje.isAfter(tempo_desconto)){
+            return ((precoCusto * (1.0 + margemLucro))*(1-DESCONTO));
+        }else{
+            return (precoCusto * (1.0 + margemLucro));
+        }
     }
-    return (precoCusto * (1 + margemLucro))*(1-desconto);
 
+    @Override
+	public String toString() {
+    	DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    	String dados = super.toString();
+        dados += "\nVálido até " + formato.format(dataDeValidade);
+        return dados;
+	}
+
+    @Override
+    public String gerarDadosTexto() {
+        DateTimeFormatter formatoData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        String dataFormatada = String.format(String.valueOf(dataDeValidade), formatoData);
+        String precoFormatado = String.format("%.2f", precoCusto).replace(',', '.');
+        String margemFormatada = String.format("%.2f", margemLucro).replace(',', '.');
+
+        String linha = String.format("2;%s;%s;%s;%s", getDescricao(), precoFormatado, margemFormatada, dataFormatada);
+        return linha;
+    }
 }
- @Override
- public String toString(){
-    DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-    String dados = super.toString();
-    dados += "\n valido ate " +formato.format(dataDeValidade);
-    return dados;
- }
-}
-
